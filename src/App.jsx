65e4be5d217fe24a49404cd390a87687f25b4adb
Ndifoin Hilary Nkaise <70800267+Ndifoinhilary@users.noreply.github.com>
2023-11-15 06:50:21 +0000

    Done with adding username and name to social media sites

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Ndifoin Hilary Nkaise",
+  title: "Computer Engineer",
+  email: "ndifoinhilary@gmail.com",
+  gitHub: "Ndifoinhilary",
+  instagram: "",
+  linkedIn: "ndifoin-hilary-nkaise-332a52256",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "HNdifoin",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
